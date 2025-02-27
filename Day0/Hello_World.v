366efd5486fee1e6844da4c3c14a128d266bb6c7
Rohit Kumar <141709960+sumitarohit@users.noreply.github.com>
2025-02-27 08:01:42 +0530

    Update Hello_World.v

--- Day0/Hello_World.v
+++ Day0/Hello_World.v
@@ -1,1 +1,9 @@
+// Code your testbench here
+// or browse Examples
+module hello_world;
+  initial begin
+    $display("Hello, World! Welcome to my 60-Day Verilog Challenge!");
+    $finish;
+  end
+endmodule
 
