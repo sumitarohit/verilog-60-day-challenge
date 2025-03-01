7226ab46f2fc48f21f77620f33f65bf8573e7bc2
Rohit Kumar <141709960+sumitarohit@users.noreply.github.com>
2025-03-01 20:38:52 +0530

    Update Logic_Gates.v

--- Day1/Logic_Gates.v
+++ Day1/Logic_Gates.v
@@ -1,1 +1,35 @@
+// AND Gate
+module and_gate (input A, input B, output Y);
+    assign Y = A & B;
+endmodule
+
+// OR Gate
+module or_gate (input A, input B, output Y);
+    assign Y = A | B;
+endmodule
+
+// NOT Gate
+module not_gate (input A, output Y);
+    assign Y = ~A;
+endmodule
+
+// NAND Gate
+module nand_gate (input A, input B, output Y);
+    assign Y = ~(A & B);
+endmodule
+
+// NOR Gate
+module nor_gate (input A, input B, output Y);
+    assign Y = ~(A | B);
+endmodule
+
+// XOR Gate
+module xor_gate (input A, input B, output Y);
+    assign Y = A ^ B;
+endmodule
+
+// XNOR Gate
+module xnor_gate (input A, input B, output Y);
+    assign Y = ~(A ^ B);
+endmodule
 
